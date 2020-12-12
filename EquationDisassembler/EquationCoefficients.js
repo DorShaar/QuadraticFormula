cea07f7672d309b233898713d985fef51a333733
Dor <Dor.Shaar@votiro.com>
2020-12-12 18:13:47 +0200

    Add implementation and non working test

---
+++ EquationDisassembler/EquationCoefficients.js
@@ -0,0 +1,8 @@
+export class EquationCoefficients {
+  constructor(equation, a, b, c) {
+    this.equation = equation;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+}
---
+++ EquationDisassembler/readme.txt
@@ -0,0 +1,5 @@
+to be able and run UT, we should install jest with yarn:
+	yarn add --dev jest
+
+to run it:
+	yarn test
