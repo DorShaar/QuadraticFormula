ca9da37f0a81de596028009a0d71a811e59987fa
Dor <Dor.Shaar@votiro.com>
2021-01-23 15:50:48 +0200

    Complete implemntation of report creation + UT + start implementing http server

--- EquationReporter/hello.go
+++ EquationReporter/hello.go
@@ -1,7 +1,22 @@
 package equationreporter
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type HttpHandler struct{}
+
+// Implement `ServeHTTP` method on `HttpHandler` struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello World!")
+	res.Write(data)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Equation Reporter started")
+
+	handler := HttpHandler{}
+
+	http.ListenAndServe(":9000", handler)
 }
